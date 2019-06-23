6c92ade35fb116297ed1baf30d9b9ba74079dc09
Lea <ltrueworthy@yahoo.com>
2019-06-23 15:10:34 -0700

    wek-8

---
+++ week-8/trueworthy_calculator.py
@@ -0,0 +1,24 @@
+
+# ============================================
+# ; Title:  trueworthy_calculator.py
+# ; Author: Professor Krasso
+# ; Date:  22 June 2019
+# ; Modified By: Lea Trueworthy
+# ; Description: Exercise 8.3- Calculator functions
+# ;===========================================
+
+first_name = "Lea"
+last_name = "Trueworthy"
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(3,2))
+print(subtract(4,3))
+print(divide(20,5))
---
+++ week-8/trueworthy_hello_world.py
@@ -0,0 +1,7 @@
+# Title: trueworthy_hello_world.py
+# Author: Professor Krasso
+# modified by: Lea Trueworthy
+# Date: 22 June 2019
+# Description: Basic demonstration of outputting values to the terminal window
+
+print('Hello World')
